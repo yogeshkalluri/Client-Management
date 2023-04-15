e094e6a5b8c38871ca78c645aafd01985897b07c
hemanth307 <90707663+hemanth307@users.noreply.github.com>
2023-04-15 02:08:01 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
             <main className="content"> 
               <Topbar /> 
               <Routes>
-                <Route path="/" element={<Dashboard />} />
+                <Route path="/ADMIN-DASHBOARD" element={<Dashboard />} />
                 <Route path="/team" element={<Team />} />
                 <Route path="/invoices" element={<Invoices />} />
                 <Route path="/contacts" element={<Contacts />} />
